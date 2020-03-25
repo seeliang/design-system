098c5c40a75fbf685aa8244a4f4d266e8e0d5564
seeliang <me@sihuiliang.com>
2020-03-25 11:06:02 +1100

    feat: add a tag

--- angularIO/atoms/button.ts
+++ angularIO/atoms/button.ts
@@ -3,8 +3,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'a-button',
   template: `
-    <button class="a-button" [ngClass]="action.type" (click)="onClick.emit($event)">{{ action.text }}</button>
-  `,
+  <ng-container [ngSwitch]="action?.html">
+    <a *ngSwitchCase="'a'" class="a-button" [ngClass]="action?.type" href="#34">{{ action?.text }}</a>
+    <button *ngSwitchDefault class="a-button" [ngClass]="action?.type" (click)="onClick.emit($event)">{{ action?.text }}</button>
+  </ng-container>
+    `,
   styleUrls: ['./button.scss']
 })
 export class Button {
