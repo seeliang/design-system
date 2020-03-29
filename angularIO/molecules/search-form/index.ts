f9ec0b7a7a7b26f96cfe7861b64fd8a260897d07
seeliang <me@sihuiliang.com>
2020-03-29 16:13:00 +1100

    chore:  typo

--- angularIO/molecules/search-form/index.ts
+++ angularIO/molecules/search-form/index.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'm-search-form',
@@ -14,7 +14,7 @@ export class SearchForm {
       text:'search',
       action: (e) => {
         e.preventDefault(); 
-        alert(`seatch for: ` + this.value)
+        alert(`search for: ` + this.value)
       }
     },
   }
